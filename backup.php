f3819fab10a676665c7a319944f93edfd80f6119
cppsgithub <sistemas@cpps-int.org>
2017-10-17 10:50:28 -0500

    Add files via upload

---
+++ backup.php
@@ -0,0 +1,14 @@
+﻿<?php
+    $curl_handle=curl_init();
+    curl_setopt($curl_handle, CURLOPT_URL, 'http://spincam3.net/index.php?option=com_akeeba&view=Backup&key=QIxymFaXAX3MJq1JMJgc8lm5nSJRTwxY');
+    curl_setopt($curl_handle,CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($curl_handle,CURLOPT_MAXREDIRS, 10000);
+    curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER, 1);
+    $buffer = curl_exec($curl_handle);
+    curl_close($curl_handle);
+    if (empty($buffer))
+        echo "Sorry, the backup didn't work.";
+    else
+        echo $buffer;
+?>
+  
